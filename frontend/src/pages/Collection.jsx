e1bd21af667c01d985fdc176ea4cd5c7e4995e86
Vuong1501 <vuong.44.10a5@gmail.com>
2025-08-22 10:12:35 +0700

    Sản phẩm liên quan

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
-    const { products } = useContext(ShopContext);
+    const { products, search, showSearch } = useContext(ShopContext);
     const [showFilter, setShowFilter] = useState(false);
     const [filterProducts, setFilterProducts] = useState([]);
     const [category, setCategory] = useState([]);
@@ -48,6 +48,9 @@ const Collection = () => {
     // hàm này để filter bên trái của COLLECTION
     const applyFilter = () => {
         let productsCopy = products.slice();
+        if (search && showSearch) {
+            productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+        }
         if (category.length > 0) {
             productsCopy = productsCopy.filter(item => category.includes(item.category));
         }
@@ -59,7 +62,7 @@ const Collection = () => {
 
     useEffect(() => {
         applyFilter();
-    }, [category, subCategory]);
+    }, [category, subCategory, search, showSearch]);
 
     useEffect(() => {
         sortProduct();
