e87af3bd2d959435c9806d639d926453ea60f07c
Vuong1501 <vuong.44.10a5@gmail.com>
2025-08-22 11:47:04 +0700

    hiển thị số lượng sản phầm trong giỏ hàng

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -8,8 +9,51 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addTocart = async (itemId, size) => {
+        // thông báo phải chọn size
+        if (!size) {
+            toast.error("Select Product Size");
+            return;
+        }
+        let cartData = structuredClone(cartItems);
+        // xem trong giỏ hàng có sản phẩm này chưa, nếu chưa thì là 1, rồi thì + thêm 1
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            else {
+                cartData[itemId][size] = 1;
+            }
+        }
+        else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        // lặp qua id của sản phẩm đó
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) {
+                    console.log("errr", error);
+                }
+            }
+        }
+        return totalCount;
+    }
+
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItems, setCartItems, addTocart,
+        getCartCount
     }
 
     return (
