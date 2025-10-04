ffd01bfcdfbf363c9a2ee3aa7780b62805828aaf
Vuong1501 <vuong.44.10a5@gmail.com>
2025-10-04 10:53:17 +0700

    tạo catogory-service

---
+++ backend/db-init/category_db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS category_db;
+USE category_db;
+
---
+++ backend/services/category-service/src/config/db.js
@@ -0,0 +1,15 @@
+// src/config/db.js
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+    process.env.MYSQL_DB,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD,
+    {
+        host: process.env.MYSQL_HOST,
+        dialect: "mysql",
+        logging: false,
+    }
+);
+
+export default sequelize;
